be64e67f0c3b0db69d75296d6dc2b5df7531de13
Tim Bakkum <tim@bikkelhart.com>
2018-03-14 19:00:58 +0100

    fixes teamActions test to use nock without error

--- src/actions/teamsActions.test.js
+++ src/actions/teamsActions.test.js
@@ -6,6 +6,7 @@ import thunk from 'redux-thunk';
 import nock from 'nock';
 import configureMockStore from 'redux-mock-store';
 import initialState from './../reducers/initialState';
+//import jsonfile from 'jsonfile';
 
 describe('Synchronous Team Actions', () => {
   describe('getTeamsSuccess', () => {
@@ -32,10 +33,10 @@ describe('Asynchronous Team Actions', () => {
 
   beforeEach(() => {
     //intercept the API call
-    // nock('https://data.sportlink.com:443')
-    //   .log(console.log)
-    //   .get('/teams?teamsoort=bond&gebruiklokaleteamgegevens=NEE&client_id=WuJeuY15Qe')
-    //   .reply(200, { body: DUMMYTEAMS });
+    nock('https://data.sportlink.com')
+      .log(console.log)
+      .get('/teams?teamsoort=bond&gebruiklokaleteamgegevens=NEE&client_id=WuJeuY15Qe')
+      .reply(200, DUMMYTEAMS);
   });
 
   it('test should create START_AJAX_CALL and GET_TEAMS_SUCCESS & END_AJAX_CALL when done', (done) => {
@@ -52,6 +53,15 @@ describe('Asynchronous Team Actions', () => {
         expect(store.getActions()).toEqual(expectedActions);
         done();
       })
-      //.catch((e) => console.error(e, null, 2));
+      // .catch((e) => {
+      //   jsonfile.writeFile('/tmp/error.json', JSON.stringify(e, null, 4), {}, (er) => {
+      //     if(er) {
+      //       console.error(er);
+      //     }
+      //     else {
+      //       console.log('file saved');
+      //     }
+      //   });
+      // });
   });
 })
